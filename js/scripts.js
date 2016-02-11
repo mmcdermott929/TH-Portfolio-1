5384a581af9e9b0085383935e845eee5bf432e9e
Mike McDermott <mmcdermott929@gmail.com>
2016-02-11 06:41:35 -0500

    Project1 1.0
    first submit to TH

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,4 @@
 $(document).ready(function(){
-//  //Jquery script to change clicked menu link to active - much simpler:
-// $('.main-nav li a').click(function(e) { // select the links    
-////     e.preventDefault(); //prevent the link from being followed
-//     $('.main-nav li a').removeClass('active'); //get rid of active class
-//     $(this).addClass('active'); // add active class to the  
-//      alert('hello');
-//  }); 
-  
 //  get the year for copyright in footer
   $(document).ready(function() {
   $("#year").text( (new Date).getFullYear() );
